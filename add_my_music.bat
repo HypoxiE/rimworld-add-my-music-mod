e9018ae2ad60afd030158c67a2a6e492fcdf9ef3
HypoxiE <kosmaer42@gmail.com>
2025-07-15 01:39:58 +0300

    Getting back to basics

--- add_my_music.bat
+++ add_my_music.bat
@@ -3,47 +3,44 @@ echo This command will completely overwrite the Songs_Gameplay.xml file of this 
 pause
 
 setlocal enabledelayedexpansion
+set "Songs_Gameplay_XML_path=Defs\SongDefs"
 
-set "localappdata=%USERPROFILE%\AppData\LocalLow"
-set "mod_user_dir=%localappdata%\Ludeon Studios\RimWorld by Ludeon Studios\AddMyMusic_data"
-if not exist "%mod_user_dir%" mkdir "%mod_user_dir%"
-
-set "music_database=%mod_user_dir%\music_is_here"
-if not exist "%music_database%" mkdir "%music_database%"
+set "music_path=Sounds\Gameplay"
 
 
 chcp 65001
-echo ^<?xml version="1.0" encoding="utf-8" ?^> > "%mod_user_dir%\Songs_Gameplay.xml"
-echo ^<Defs^> >> "%mod_user_dir%\Songs_Gameplay.xml"
+echo ^<?xml version="1.0" encoding="utf-8" ?^> > "%Songs_Gameplay_XML_path%\Songs_Gameplay.xml"
+echo ^<Defs^> >> "%Songs_Gameplay_XML_path%\Songs_Gameplay.xml"
 
-for %%f in ("%music_database%\*") do (
+for %%f in ("%music_path%\*") do (
 	set sound_name=%%~nf
 
-	echo ^<SongDef^> >> "%mod_user_dir%\Songs_Gameplay.xml"
-	echo 	^<defName^>!sound_name!^</defName^> >> "%mod_user_dir%\Songs_Gameplay.xml"
-	echo 	^<clipPath^>Gameplay/!sound_name!^</clipPath^> >> "%mod_user_dir%\Songs_Gameplay.xml"
-	echo. >> "%mod_user_dir%\Songs_Gameplay.xml"
-	echo 	^<^^!--The probability of music appearing in the game--^> >> "%mod_user_dir%\Songs_Gameplay.xml"
-	echo 	^<commonality^>1.0^</commonality^> >> "%mod_user_dir%\Songs_Gameplay.xml"
-	echo. >> "%mod_user_dir%\Songs_Gameplay.xml"
-	echo 	^<^^!--Music volume--^> >> "%mod_user_dir%\Songs_Gameplay.xml"
-	echo 	^<volume^>1.0^</volume^> >> "%mod_user_dir%\Songs_Gameplay.xml"
-	echo. >> "%mod_user_dir%\Songs_Gameplay.xml"
-	echo 	^<^^!--If true, it only plays in a combat situation--^> >> "%mod_user_dir%\Songs_Gameplay.xml"
-	echo 	^<tense^>false^</tense^> >> "%mod_user_dir%\Songs_Gameplay.xml"
-	echo. >> "%mod_user_dir%\Songs_Gameplay.xml"
-	echo 	^<^^!--If you want to limit the time of day, delete '^<^^!- -' and '- -^>' of the desired option--^> >> "%mod_user_dir%\Songs_Gameplay.xml"
-	echo 	^<^^!--^<allowedTimeOfDay^>Day^</allowedTimeOfDay^>--^> >> "%mod_user_dir%\Songs_Gameplay.xml"
-	echo 	^<^^!--^<allowedTimeOfDay^>Night^</allowedTimeOfDay^>--^> >> "%mod_user_dir%\Songs_Gameplay.xml"
-	echo. >> "%mod_user_dir%\Songs_Gameplay.xml"
-	echo 	^<^^!--Listing the seasons in which this song is played--^> >> "%mod_user_dir%\Songs_Gameplay.xml"
-	echo 	^<allowedSeasons^> ^<li^>Winter^</li^>^<li^>Spring^</li^>^<li^>Summer^</li^>^<li^>Fall^</li^> ^</allowedSeasons^> >> "%mod_user_dir%\Songs_Gameplay.xml"
+	echo ^<SongDef^> >> "%Songs_Gameplay_XML_path%\Songs_Gameplay.xml"
+	echo 	^<defName^>!sound_name!^</defName^> >> "%Songs_Gameplay_XML_path%\Songs_Gameplay.xml"
+	echo 	^<clipPath^>Gameplay/!sound_name!^</clipPath^> >> "%Songs_Gameplay_XML_path%\Songs_Gameplay.xml"
+	echo. >> "%Songs_Gameplay_XML_path%\Songs_Gameplay.xml"
+	echo 	^<^^!--The probability of music appearing in the game--^> >> "%Songs_Gameplay_XML_path%\Songs_Gameplay.xml"
+	echo 	^<commonality^>1.0^</commonality^> >> "%Songs_Gameplay_XML_path%\Songs_Gameplay.xml"
+	echo. >> "%Songs_Gameplay_XML_path%\Songs_Gameplay.xml"
+	echo 	^<^^!--Music volume--^> >> "%Songs_Gameplay_XML_path%\Songs_Gameplay.xml"
+	echo 	^<volume^>1.0^</volume^> >> "%Songs_Gameplay_XML_path%\Songs_Gameplay.xml"
+	echo. >> "%Songs_Gameplay_XML_path%\Songs_Gameplay.xml"
+	echo 	^<^^!--If true, it only plays in a combat situation--^> >> "%Songs_Gameplay_XML_path%\Songs_Gameplay.xml"
+	echo 	^<tense^>false^</tense^> >> "%Songs_Gameplay_XML_path%\Songs_Gameplay.xml"
+	echo. >> "%Songs_Gameplay_XML_path%\Songs_Gameplay.xml"
+	echo 	^<^^!--If you want to limit the time of day, delete '^<^^!- -' and '- -^>' of the desired option--^> >> "%Songs_Gameplay_XML_path%\Songs_Gameplay.xml"
+	echo 	^<^^!--^<allowedTimeOfDay^>Day^</allowedTimeOfDay^>--^> >> "%Songs_Gameplay_XML_path%\Songs_Gameplay.xml"
+	echo 	^<^^!--^<allowedTimeOfDay^>Night^</allowedTimeOfDay^>--^> >> "%Songs_Gameplay_XML_path%\Songs_Gameplay.xml"
+	echo. >> "%Songs_Gameplay_XML_path%\Songs_Gameplay.xml"
+	echo 	^<^^!--Listing the seasons in which this song is played--^> >> "%Songs_Gameplay_XML_path%\Songs_Gameplay.xml"
+	echo 	^<allowedSeasons^> ^<li^>Winter^</li^>^<li^>Spring^</li^>^<li^>Summer^</li^>^<li^>Fall^</li^> ^</allowedSeasons^> >> "%Songs_Gameplay_XML_path%\Songs_Gameplay.xml"
 
-	echo ^</SongDef^> >> "%mod_user_dir%\Songs_Gameplay.xml"
-	echo. >> "%mod_user_dir%\Songs_Gameplay.xml"
+	echo ^</SongDef^> >> "%Songs_Gameplay_XML_path%\Songs_Gameplay.xml"
+	echo. >> "%Songs_Gameplay_XML_path%\Songs_Gameplay.xml"
+	echo Added !sound_name!
 )
 
-echo ^</Defs^> >> "%mod_user_dir%\Songs_Gameplay.xml"
+echo ^</Defs^> >> "%Songs_Gameplay_XML_path%\Songs_Gameplay.xml"
 
 echo Songs_Gameplay.xml overwritten. After restarting the game, music will be added.
 pause
